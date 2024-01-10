aff734a455c18b14933cca1d4c35de65bf8a2cf7
rsuwan <r.r.suwan2001@gmail.com>
2024-01-10 21:09:18 +0200

    kk

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import 'dotenv/config';
-import express from 'express';
-import initApp from './src/modules/app.router.js';
+import "dotenv/config";
+import express from "express";
+import initApp from "./src/modules/app.router.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-initApp(app,express);
+initApp(app, express);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
