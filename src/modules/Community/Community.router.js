55735e5d8e2a81963ce6e1047e9ed7891dc7d16d
batool-saleh01 <155902900+batool-saleh01@users.noreply.github.com>
2024-01-10 10:01:28 +0200

    Update Community.router.js

--- src/modules/Community/Community.router.js
+++ src/modules/Community/Community.router.js
@@ -2,7 +2,10 @@ import { Router } from "express";
 import * as communitycontroller from './community.controller.js';
 const router = Router();
 
-router.get("/", communitycontroller.getcommunity);
+router.post("/createCommunity", communitycontroller.createCommunity);
+router.post("/addProperty", communitycontroller.addProperty);
+router.post("/viewProperty", communitycontroller.viewProperty);
+router.post("/removeProperty", communitycontroller.removeProperty);
 
 export default router;
 
