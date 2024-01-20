456e5b97861fe8f4b6f532951f9e8a3f90cc688c
rsuwan <r.r.suwan2001@gmail.com>
2024-01-20 21:38:50 +0200

    raghad172838

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import initApp from './src/modules/app.router.js';
+import cors from 'cors'; 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 initApp(app,express);
 
