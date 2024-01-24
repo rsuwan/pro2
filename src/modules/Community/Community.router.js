fd82a4a986b43dda41117b5db44227e04bf7f05e
rsuwan <r.r.suwan2001@gmail.com>
2024-01-24 18:56:40 +0200

    get

--- src/modules/Community/Community.router.js
+++ src/modules/Community/Community.router.js
@@ -12,7 +12,7 @@ router.get("/getCommunities", communitycontroller.getCommunities);
 router.get("/getactiveCommunities", communitycontroller.getActiveCommunities);
 //router.get("/:id", communitycontroller.getSpecificCommunity);
 router.post("/:community_name/addProperty", communitycontroller.addProperty);
-router.post("/:community_name/viewProperty", communitycontroller.viewProperty);
+router.get("/:community_name/viewProperty", communitycontroller.viewProperty);
 router.delete("/:community_name/deleteCommunity", communitycontroller.deleteCommunity);
 
 router.post("/cancleCreation", communitycontroller.cancleCreation);
