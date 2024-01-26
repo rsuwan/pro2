0101aef438339fa47275e1db6081e2f773258f02
rsuwan <r.r.suwan2001@gmail.com>
2024-01-26 15:44:12 +0200

    parto

--- src/modules/communitys/communitys.router.js
+++ src/modules/communitys/communitys.router.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
-import * as communityscntroller from './communities.control.js';
-import fileUpload, { fileValidation } from "../../services/multer.js";
-const router = Router();
+// import { Router } from "express";
+// import * as communityscntroller from './communities.control.js';
+// import fileUpload, { fileValidation } from "../../services/multer.js";
+// const router = Router();
 
-router.post("/createCommunitys", fileUpload(fileValidation.image).fields([
-{name:'mainImage',maxCount:1},
-{name:'supImages',maxCount:30},
-]),communityscntroller.createCommunitys);
-export default router;
+// router.post("/createCommunitys", fileUpload(fileValidation.image).fields([
+// {name:'mainImage',maxCount:1},
+// {name:'supImages',maxCount:30},
+// ]),communityscntroller.createCommunitys);
+// // export default router;
