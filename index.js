108bdae01da87fb11248931ec9e085373492f3cb
rsuwan <r.r.suwan2001@gmail.com>
2024-01-28 18:25:13 +0200

    withot comp

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// server.js
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import compression from 'compression';
+//import compression from 'compression';
 import initApp from './src/modules/app.router.js';
-import connectdb from './db/connection.js';
 
 const app = express();
 app.use(cors());
-app.use(compression());
+//app.use(compression());
 
 initApp(app, express);
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
