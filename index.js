80c290325a7c65cd0bcd4861f98860bfddf6909e
rsuwan <r.r.suwan2001@gmail.com>
2024-01-28 17:49:19 +0200

    port

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ import initApp from './src/modules/app.router.js';
 import connectdb from './db/connection.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(compression());
 
 initApp(app, express);
-
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
